ae4f0e790c2acb1ff5b1856352b73148362bd4f0
Kyuhwa Lee <lee.kyuh@gmail.com>
2019-01-30 15:40:19 +0100

    Python 3 compatible

--- python/python_client.py
+++ python/python_client.py
@@ -1,105 +1,316 @@
-from __future__ import print_function
+from __future__ import print_function, division
 
 """
-serial_writer.py
+Send trigger events to parallel port.
 
-Send 1-byte number to a serial port.
-Supports dummy port for testing without actual device.
+See sample code at the end.
 
-Kyuhwa Lee, 2016
-EPFL
+Kyuhwa Lee, 2014
+Swiss Federal Institute of Technology Lausanne (EPFL)
 
 """
 
+import pycnbi.utils.cnbi_lsl as cnbi_lsl
+import pycnbi.utils.pycnbi_utils as pu
+import pycnbi.utils.q_common as qc
+import pylsl
+import threading
+import os
 import sys
-import serial, serial.tools.list_ports
+import ctypes
+import time
+from builtins import input, bytes
 
-class SerialWriter:
-	def __init__(self, serial_port, baud_rate=115200):
-		"""
-		Input
-		-----
-		serial_port: int | string | None
-			int: use a fixed COM port number.
-			string: search with partial device name.
-			None: show ports list and create a dummy port.
-		baud_rate: int
-			baud rate for serial communication
+class Trigger(object):
+    """
+    Supported device types:
+     'Arduino': CNBI Arduino trigger
+     'USB2LPT': Commercial USB2LPT adapter
+     'DESKTOP': Desktop native LPT
+     'SOFTWARE': Software trigger
+     'FAKE': Mock trigger device for testing
 
-		Note
-		----
-		Arduino Mega and Micro can be searched with device name but
-		Nano doesn't seem to work.
-		"""
-		self.comport= None
+    When using USB2LPT, the port number (e.g. 0x378) can be searched automatically.
+    When using Desktop's LPT, the port number must be specified during initialization.
 
-		if serial_port is None:
-			self.list_ports()
-		elif type(serial_port)==str:
-			ports= [x for x in serial.tools.list_ports.grep(serial_port)]
-			if len(ports) == 0:
-				raise RuntimeError, 'Cannot find %s'% serial_port
-			else:
-				if len(ports) > 1:
-					print('WARNING: More than one device detected.')
-				for i,a in enumerate(ports):
-					print('Found %s'% a )
-				# just use the first one
-				self.comport= ports[0].device
-		elif type(serial_port)==int:
-			self.comport= 'COM%d'% serial_port
-		else:
-			raise RuntimeError, 'Unrecognized name %s %s'% (serial_port,type(serial_port))
+    Software trigger writes event information into a text file with LSL timestamps, which
+    can be later added to fif. This file will be automatically saved and closed when Ctrl+C is
+    pressed or terminal window is closed (or killed for whatever reason).
 
-		if self.comport is not None:
-			self.ser= serial.Serial(self.comport, baud_rate)
-			print('Connected to %s.'% self.comport)
-		else:
-			self.ser= None
-			self.write= self.write_dummy
-			self.close= self.close_dummy
-			print('**** WARNING: Connected to a dummy serial port. ****\n')
+    The asynchronous function signal(x) sends 1-byte integer value x and returns immediately.
+    It schedules to send the value 0 at the end of the signal length.
 
-	def list_ports(self):
-		print('--- List of active COM ports ---')
-		for p in [x.description for x in serial.tools.list_ports.comports()]:
-			print(p)
-		print()
+    To use with USB2LPT, download the driver from:
+    https://www-user.tu-chemnitz.de/~heha/bastelecke/Rund%20um%20den%20PC/USB2LPT/index.en.htm
 
-	def write(self, number):
-		return self.ser.write( chr(number) )
+    I've made a C++ library to send commands to LPTx using standard Windows API.
+    Use LptControl64.dll for 64 bit Python and LptControl32.dll for 32 bit Python.
 
-	def close(self):
-		print('\nClosing serial port.')
-		self.ser.close()
-		self.ser= None
+    Some important functions:
+    int init(duration)
+        Returns False if error, True if success.
+        Duration unit: msec
 
-	def is_dummy(self):
-		return self.ser is None
+    void signal(value)
+        Sends the value to the parallel port and sets to 0 after a set period.
+        The value shuold be an integer in the range of 0-255.
+    """
+    def __init__(self, lpttype='USB2LPT', portaddr=None, verbose=True):
+        self.evefile = None
+        self.lpttype = lpttype
+        self.verbose = verbose
 
-	def write_dummy(self, number):
-		return len( chr(number) )
+        if self.lpttype in ['USB2LPT', 'DESKTOP']:
+            if self.lpttype == 'USB2LPT':
+                if ctypes.sizeof(ctypes.c_voidp) == 4:
+                    dllname = 'LptControl_USB2LPT32.dll'  # 32 bit
+                else:
+                    dllname = 'LptControl_USB2LPT64.dll'  # 64 bit
+                if portaddr not in [0x278, 0x378]:
+                    self.print('Warning: LPT port address %d is unusual.' % portaddr)
 
-	def close_dummy(self):
-		print('\nClosing dummy serial port.')
+            elif self.lpttype == 'DESKTOP':
+                if ctypes.sizeof(ctypes.c_voidp) == 4:
+                    dllname = 'LptControl_Desktop32.dll'  # 32 bit
+                else:
+                    dllname = 'LptControl_Desktop64.dll'  # 64 bit
+                if portaddr not in [0x278, 0x378]:
+                    self.print('Warning: LPT port address %d is unusual.' % portaddr)
 
-# sample code
+            self.portaddr = portaddr
+            search = []
+            search.append(os.path.dirname(__file__) + '/' + dllname)
+            search.append(os.path.dirname(__file__) + '/libs/' + dllname)
+            search.append(os.getcwd() + '/' + dllname)
+            search.append(os.getcwd() + '/libs/' + dllname)
+            for f in search:
+                if os.path.exists(f):
+                    dllpath = f
+                    break
+            else:
+                self.print('ERROR: Cannot find the required library %s' % dllname)
+                raise RuntimeError
+
+            self.print('Loading %s' % dllpath)
+            self.lpt = ctypes.cdll.LoadLibrary(dllpath)
+
+        elif self.lpttype == 'ARDUINO':
+            import serial, serial.tools.list_ports
+            BAUD_RATE = 115200
+
+            # portaddr should be None or in the form of 'COM1', 'COM2', etc.
+            if portaddr is None:
+                arduinos = [x for x in serial.tools.list_ports.grep('Arduino')]
+                if len(arduinos) == 0:
+                    print('No Arduino found. Stop.')
+                    sys.exit()
+
+                for i, a in enumerate(arduinos):
+                    print('Found', a[0])
+                try:
+                    com_port = arduinos[0].device
+                except AttributeError: # depends on Python distribution
+                    com_port = arduinos[0][0]
+            else:
+                com_port = portaddr
+
+            self.ser = serial.Serial(com_port, BAUD_RATE)
+            time.sleep(1)  # doesn't work without this delay. why?
+            print('Connected to %s.' % com_port)
+
+        elif self.lpttype == 'SOFTWARE':
+            from pycnbi.stream_receiver.stream_receiver import StreamReceiver
+            self.print('Using software trigger')
+
+            # get data file location
+            LSL_SERVER = 'StreamRecorderInfo'
+            inlet = cnbi_lsl.start_client(LSL_SERVER)
+            fname = inlet.info().source_id()
+            if fname[-4:] != '.pcl':
+                self.print('ERROR: Received wrong record file name format %s' % fname)
+                sys.exit(-1)
+            evefile = fname[:-8] + '-eve.txt'
+            eveoffset_file = fname[:-8] + '-eve-offset.txt'
+            self.print('Event file is: %s' % evefile)
+            self.evefile = open(evefile, 'a')
+
+            # check server LSL time server integrity
+            self.print("Checking LSL server's timestamp integrity for logging software triggers.")
+            amp_name, amp_serial = pu.search_lsl()
+            sr = StreamReceiver(window_size=1, buffer_size=1, amp_serial=amp_serial, eeg_only=False, amp_name=amp_name)
+            local_time = pylsl.local_clock()
+            server_time = sr.get_window_list()[1][-1]
+            lsl_time_offset = local_time - server_time
+            with open(eveoffset_file, 'a') as f:
+                f.write('Local time: %.6f, Server time: %.6f, Offset: %.6f\n' % (local_time, server_time, lsl_time_offset))
+            self.print('LSL timestamp offset (%.3f) saved to %s' % (lsl_time_offset, eveoffset_file))
+
+        elif self.lpttype == 'FAKE' or self.lpttype is None or self.lpttype is False:
+            self.print('WARNING: Using a fake trigger.')
+            self.lpttype = 'FAKE'
+            self.lpt = None
+
+        else:
+            self.print('ERROR: Unknown LPT port type %s' % lpttype)
+            sys.exit(-1)
+
+    def __del__(self):
+        if self.evefile is not None and not self.evefile.closed:
+            self.evefile.close()
+            self.print('Event file saved.')
+            sys.stdout.flush()
+
+    def print(self, *args):
+        qc.print_c('[pyLptControl] ', color='w', end='')
+        print(*args)
+
+    def init(self, duration):
+        if self.lpttype == 'SOFTWARE':
+            self.print('>> Ignoring delay parameter for software trigger')
+            return True
+        elif self.lpttype == 'FAKE':
+            return True
+        else:
+            self.delay = duration / 1000.0
+
+            if self.lpttype in ['DESKTOP', 'USB2LPT']:
+                if self.lpt.init() == -1:
+                    self.print('Connecting to LPT port failed. Check the driver status.')
+                    self.lpt = None
+                    return False
+
+            self.action = False
+            self.offtimer = threading.Timer(self.delay, self.signal_off)
+            return True
+
+    # write to software trigger
+    def write_event(self, value):
+        assert self.lpttype == 'SOFTWARE'
+        self.evefile.write('%.6f\t0\t%d\n' % (pylsl.local_clock(), value))
+        return True
+
+    # set data
+    def set_data(self, value):
+        if self.lpttype == 'SOFTWARE':
+            self.print('>> set_data() not supported for software trigger.')
+            return False
+        elif self.lpttype == 'FAKE':
+            self.print('FAKE trigger value', value)
+            return True
+        else:
+            if self.lpttype == 'USB2LPT':
+                self.lpt.setdata(value)
+            elif self.lpttype == 'DESKTOP':
+                self.lpt.setdata(self.portaddr, value)
+            elif self.lpttype == 'ARDUINO':
+                self.ser.write(bytes([value]))
+            else:
+                raise RuntimeError('Wrong trigger device')
+
+    # sends data and turn off after delay
+    def signal(self, value):
+        if self.lpttype == 'SOFTWARE':
+            if self.verbose is True:
+                self.print('Sending software trigger', value)
+            return self.write_event(value)
+        elif self.lpttype == 'FAKE':
+            self.print('Sending FAKE trigger signal', value)
+            return True
+        else:
+            if self.offtimer.is_alive():
+                self.print('Warning: You are sending a new signal before the end of the last signal. Signal ignored.')
+                self.print('self.delay=%.1f' % self.delay)
+                return False
+            self.set_data(value)
+            if self.verbose is True:
+                self.print('Sending', value)
+            self.offtimer.start()
+            return True
+
+    # set data to zero (all bits off)
+    def signal_off(self):
+        if self.lpttype == 'SOFTWARE':
+            return self.write_event(0)
+        elif self.lpttype == 'FAKE':
+            self.print('FAKE trigger off')
+            return True
+        else:
+            self.set_data(0)
+            self.offtimer = threading.Timer(self.delay, self.signal_off)
+
+    # set pin
+    def set_pin(self, pin):
+        if self.lpttype == 'SOFTWARE':
+            self.print('>> set_pin() not supported for software trigger.')
+            return False
+        elif self.lpttype == 'FAKE':
+            self.print('FAKE trigger pin', pin)
+            return True
+        else:
+            self.set_data(2 ** (pin - 1))
+
+
+class MockTrigger(object):
+    def __init__(self):
+        self.print('*' * 50)
+        self.print(' WARNING: MockTrigger class is deprecated.')
+        self.print("          Use Trigger('FAKE') instead.")
+        self.print('*' * 50 + '\n')
+
+    def init(self, duration=100):
+        self.print('Mock Trigger ready')
+        return True
+
+    def print(self, *args):
+        print('[pyLptControl] ', end='')
+        print(*args)
+
+    def signal(self, value):
+        self.print('FAKE trigger signal', value)
+        return Trues
+
+    def signal_off(self):
+        self.print('FAKE trigger value 0')
+        return True
+
+    def set_data(self, value):
+        self.print('FAKE trigger value', value)
+        return True
+
+    def set_pin(self, pin):
+        self.print('FAKE trigger pin', pin)
+        return True
+
+
+# set 1 to each bit and rotate from bit 0 to bit 7
+def test_all_bits(trigger):
+    for x in range(8):
+        val = 2 ** x
+        trigger.signal(val)
+        #trigger.set_data(val)
+        print(val)
+        time.sleep(1)
+
+# sample test code
 if __name__ == '__main__':
-	SERIAL_PORT= 'Arduino Micro'
-	#SERIAL_PORT= None # test with a dummy device
-	BAUD_RATE= 115200
+    #trigger = Trigger('COM3') # Arduino trigger
+    trigger = Trigger('ARDUINO') # Arduino trigger
+    if not trigger.init(500):
+        print('LPT port cannot be opened. Using mock trigger.')
+        trigger = MockTrigger()
 
-	sw= SerialWriter(SERIAL_PORT, BAUD_RATE)
+    # Christmas tree mode (set 1 bit-by-bit)
+    #test_all_bits(trigger)
 
-	print('Enter an integer between 0 and 255. Ctrl+C to stop.')
-	while True:
-		try:
-			data= int( raw_input('Input? ').strip() )
-			assert 0 <= data <= 255
-			sw.write(data)
-			print('Sent', data)
-		except KeyboardInterrupt:
-			break
-
-	sw.close()
-	print('Done.')
+    print('Type quit or Ctrl+C to finish.')
+    while True:
+        val = input('Trigger value? ')
+        if val.strip() == '':
+            continue
+        if val == 'quit':
+            break
+        if 0 <= int(val) <= 255:
+            trigger.signal(int(val))
+            print('Sent %d' % int(val))
+        else:
+            print('Ignored %s' % val)
